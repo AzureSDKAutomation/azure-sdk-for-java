9f4e02204584015f2712853a0dbb7acdc07e9c26
SDK Automation <sdkautomation@microsoft.com>
2020-05-07 21:06:18 +0000

    Generated from 7b979641fd7f464632d9608bbfb6701fa3c60f1b
    fix model validation

--- sdk/search/mgmt-resource-manager/v2020_03_13/src/main/java/com/microsoft/azure/management/search/v2020_03_13/NetworkRuleSet.java
+++ sdk/search/mgmt-resource-manager/v2020_03_13/src/main/java/com/microsoft/azure/management/search/v2020_03_13/NetworkRuleSet.java
@@ -17,15 +17,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  */
 public class NetworkRuleSet {
     /**
-     * A list of IP restriction rules that defines the inbound network access
-     * to the search service endpoint. These restriction rules are applied only
-     * when the EndpointAccess of the search service is Public.
+     * A list of IP restriction rules that defines the inbound network(s) with
+     * allowing access to the search service endpoint. At the meantime, all
+     * other public IP networks are blocked by the firewall. These restriction
+     * rules are applied only when the 'publicNetworkAccess' of the search
+     * service is 'enabled'; otherwise, traffic over public interface is not
+     * allowed even with any public IP rules, and private endpoint connections
+     * would be the exclusive access method.
      */
     @JsonProperty(value = "ipRules")
     private List<IpRule> ipRules;
 
     /**
-     * Get a list of IP restriction rules that defines the inbound network access to the search service endpoint. These restriction rules are applied only when the EndpointAccess of the search service is Public.
+     * Get a list of IP restriction rules that defines the inbound network(s) with allowing access to the search service endpoint. At the meantime, all other public IP networks are blocked by the firewall. These restriction rules are applied only when the 'publicNetworkAccess' of the search service is 'enabled'; otherwise, traffic over public interface is not allowed even with any public IP rules, and private endpoint connections would be the exclusive access method.
      *
      * @return the ipRules value
      */
@@ -34,7 +38,7 @@ public class NetworkRuleSet {
     }
 
     /**
-     * Set a list of IP restriction rules that defines the inbound network access to the search service endpoint. These restriction rules are applied only when the EndpointAccess of the search service is Public.
+     * Set a list of IP restriction rules that defines the inbound network(s) with allowing access to the search service endpoint. At the meantime, all other public IP networks are blocked by the firewall. These restriction rules are applied only when the 'publicNetworkAccess' of the search service is 'enabled'; otherwise, traffic over public interface is not allowed even with any public IP rules, and private endpoint connections would be the exclusive access method.
      *
      * @param ipRules the ipRules value to set
      * @return the NetworkRuleSet object itself.
