bca433e961472b09926bff17ef180815df5445e3
SDKAuto <sdkautomation@microsoft.com>
2021-02-18 09:06:10 +0000

    CodeGen from PR 13012 in Azure/azure-rest-api-specs Merge 1ebde9caa4e68859a99b15cdf45aaff0a2001664 into c740df72bd77fd8c6a822bbd1ad8d366a31dc2b3

--- sdk/loganalytics/azure-resourcemanager-loganalytics/src/main/java/com/azure/resourcemanager/loganalytics/models/WorkspacePatch.java
+++ sdk/loganalytics/azure-resourcemanager-loganalytics/src/main/java/com/azure/resourcemanager/loganalytics/models/WorkspacePatch.java
@@ -68,6 +68,13 @@ public class WorkspacePatch extends AzureEntityResource {
     private PublicNetworkAccessType publicNetworkAccessForQuery;
 
     /*
+     * Indicates whether customer managed storage is mandatory for query
+     * management.
+     */
+    @JsonProperty(value = "properties.forceCmkForQuery")
+    private Boolean forceCmkForQuery;
+
+    /*
      * List of linked private link scope resources.
      */
     @JsonProperty(value = "properties.privateLinkScopedResources", access = JsonProperty.Access.WRITE_ONLY)
@@ -219,6 +226,26 @@ public class WorkspacePatch extends AzureEntityResource {
      */
     public WorkspacePatch withPublicNetworkAccessForQuery(PublicNetworkAccessType publicNetworkAccessForQuery) {
         this.publicNetworkAccessForQuery = publicNetworkAccessForQuery;
+        return this;
+    }
+
+    /**
+     * Get the forceCmkForQuery property: Indicates whether customer managed storage is mandatory for query management.
+     *
+     * @return the forceCmkForQuery value.
+     */
+    public Boolean forceCmkForQuery() {
+        return this.forceCmkForQuery;
+    }
+
+    /**
+     * Set the forceCmkForQuery property: Indicates whether customer managed storage is mandatory for query management.
+     *
+     * @param forceCmkForQuery the forceCmkForQuery value to set.
+     * @return the WorkspacePatch object itself.
+     */
+    public WorkspacePatch withForceCmkForQuery(Boolean forceCmkForQuery) {
+        this.forceCmkForQuery = forceCmkForQuery;
         return this;
     }
 
