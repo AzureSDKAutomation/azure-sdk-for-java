90717f167c78ecb10b62d8ddfd239d794c3101d0
SDKAuto <sdkautomation@microsoft.com>
2021-04-21 01:55:42 +0000

    CodeGen from PR 13967 in Azure/azure-rest-api-specs Merge 21a64a4c9eb2977b5e9ee8cba700e0a52e3635ff into 8e938ae5e8b877da09ff207850febb2645c2ac4c

--- sdk/consumption/azure-resourcemanager-consumption/src/main/java/com/azure/resourcemanager/consumption/models/EventsOperations.java
+++ sdk/consumption/azure-resourcemanager-consumption/src/main/java/com/azure/resourcemanager/consumption/models/EventsOperations.java
@@ -12,35 +12,31 @@ public interface EventsOperations {
     /**
      * Lists the events by billingAccountId and billingProfileId for given start and end date.
      *
+     * @param billingAccountId BillingAccount ID.
+     * @param billingProfileId Azure Billing Profile ID.
      * @param startDate Start date.
      * @param endDate End date.
-     * @param scope The scope associated with events operations. This includes
-     *     '/providers/Microsoft.Billing/billingAccounts/{billingAccountId}/billingProfile/{billingProfileId}' for
-     *     Billing Profile scope, and
-     *     'providers/Microsoft.Billing/billingAccounts/{billingAccountId}/customers/{customerId}' specific for
-     *     partners.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
      * @return result of listing event summary.
      */
-    PagedIterable<EventSummary> list(String startDate, String endDate, String scope);
+    PagedIterable<EventSummary> list(
+        String billingAccountId, String billingProfileId, String startDate, String endDate);
 
     /**
      * Lists the events by billingAccountId and billingProfileId for given start and end date.
      *
+     * @param billingAccountId BillingAccount ID.
+     * @param billingProfileId Azure Billing Profile ID.
      * @param startDate Start date.
      * @param endDate End date.
-     * @param scope The scope associated with events operations. This includes
-     *     '/providers/Microsoft.Billing/billingAccounts/{billingAccountId}/billingProfile/{billingProfileId}' for
-     *     Billing Profile scope, and
-     *     'providers/Microsoft.Billing/billingAccounts/{billingAccountId}/customers/{customerId}' specific for
-     *     partners.
      * @param context The context to associate with this operation.
      * @throws IllegalArgumentException thrown if parameters fail the validation.
      * @throws com.azure.core.management.exception.ManagementException thrown if the request is rejected by server.
      * @throws RuntimeException all other wrapped checked exceptions if the request fails to be sent.
      * @return result of listing event summary.
      */
-    PagedIterable<EventSummary> list(String startDate, String endDate, String scope, Context context);
+    PagedIterable<EventSummary> list(
+        String billingAccountId, String billingProfileId, String startDate, String endDate, Context context);
 }
