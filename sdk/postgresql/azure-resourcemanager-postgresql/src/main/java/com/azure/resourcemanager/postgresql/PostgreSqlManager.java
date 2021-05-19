16cf72990bf36dd0873d76a332b8d9ef34217499
SDKAuto <sdkautomation@microsoft.com>
2021-05-19 00:10:57 +0000

    CodeGen from PR 14389 in Azure/azure-rest-api-specs Merge 8fd3ab4709bfe53c8f365876ee9736dded054c6a into e0624d2b17a8dcee07acfb08f39508a773a8dc21

--- sdk/postgresql/azure-resourcemanager-postgresql/src/main/java/com/azure/resourcemanager/postgresql/PostgreSqlManager.java
+++ sdk/postgresql/azure-resourcemanager-postgresql/src/main/java/com/azure/resourcemanager/postgresql/PostgreSqlManager.java
@@ -230,7 +230,7 @@ public final class PostgreSqlManager {
                 .append("-")
                 .append("com.azure.resourcemanager.postgresql")
                 .append("/")
-                .append("1.0.0");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
