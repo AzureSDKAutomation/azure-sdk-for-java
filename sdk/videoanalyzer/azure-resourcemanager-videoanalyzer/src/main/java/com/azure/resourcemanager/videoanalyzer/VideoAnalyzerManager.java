e4e36da3a8dceed8a4717fd69c0cc1bcdea20161
SDKAuto <sdkautomation@microsoft.com>
2021-05-25 01:39:20 +0000

    CodeGen from PR 14526 in Azure/azure-rest-api-specs mgmt, video analyzer java, update readme and description (#14526)

--- sdk/videoanalyzer/azure-resourcemanager-videoanalyzer/src/main/java/com/azure/resourcemanager/videoanalyzer/VideoAnalyzerManager.java
+++ sdk/videoanalyzer/azure-resourcemanager-videoanalyzer/src/main/java/com/azure/resourcemanager/videoanalyzer/VideoAnalyzerManager.java
@@ -181,7 +181,7 @@ public final class VideoAnalyzerManager {
                 .append("-")
                 .append("com.azure.resourcemanager.videoanalyzer")
                 .append("/")
-                .append("1.0.0-beta.2");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
