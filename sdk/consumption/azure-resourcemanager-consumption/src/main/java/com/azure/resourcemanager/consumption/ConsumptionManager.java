c0dccc3e803807aa8e61e3e8dd88fd890d5b697a
SDKAuto <sdkautomation@microsoft.com>
2021-07-12 20:41:18 +0000

    CodeGen from PR 15160 in Azure/azure-rest-api-specs Merge 2fcd8ae2c3eeb7e55820e1f51de99d09b742373c into 0e23428515973e7893a9e6364f0e432f1e8adeb5

--- sdk/consumption/azure-resourcemanager-consumption/src/main/java/com/azure/resourcemanager/consumption/ConsumptionManager.java
+++ sdk/consumption/azure-resourcemanager-consumption/src/main/java/com/azure/resourcemanager/consumption/ConsumptionManager.java
@@ -241,7 +241,7 @@ public final class ConsumptionManager {
                 .append("-")
                 .append("com.azure.resourcemanager.consumption")
                 .append("/")
-                .append("1.0.0-beta.2");
+                .append("1.0.0-beta.1");
             if (!Configuration.getGlobalConfiguration().get("AZURE_TELEMETRY_DISABLED", false)) {
                 userAgentBuilder
                     .append(" (")
