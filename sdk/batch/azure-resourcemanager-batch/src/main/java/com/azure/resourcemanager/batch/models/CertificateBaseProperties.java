b4f122d903849386a05cdf7c96b6e8c04b228c67
SDKAuto <sdkautomation@microsoft.com>
2021-07-12 20:30:43 +0000

    CodeGen from PR 15160 in Azure/azure-rest-api-specs Merge 2fcd8ae2c3eeb7e55820e1f51de99d09b742373c into 0e23428515973e7893a9e6364f0e432f1e8adeb5

--- sdk/batch/azure-resourcemanager-batch/src/main/java/com/azure/resourcemanager/batch/models/CertificateBaseProperties.java
+++ sdk/batch/azure-resourcemanager-batch/src/main/java/com/azure/resourcemanager/batch/models/CertificateBaseProperties.java
@@ -9,7 +9,7 @@ import com.azure.core.util.logging.ClientLogger;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-/** The CertificateBaseProperties model. */
+/** Base certificate properties. */
 @Fluent
 public class CertificateBaseProperties {
     @JsonIgnore private final ClientLogger logger = new ClientLogger(CertificateBaseProperties.class);
