6a82a796f053295e63732ac8eed3f6f66c8be216
SDKAuto <sdkautomation@microsoft.com>
2021-07-12 04:07:52 +0000

    CodeGen from PR 15149 in Azure/azure-rest-api-specs Merge 6864adf0715ccefeda4a82f74ff098cc7bc77728 into 5e85541d5c4d00459a8e2dcb14512c964e2c1ce9

--- sdk/databox/azure-resourcemanager-databox/src/main/java/com/azure/resourcemanager/databox/models/HeavyScheduleAvailabilityRequest.java
+++ sdk/databox/azure-resourcemanager-databox/src/main/java/com/azure/resourcemanager/databox/models/HeavyScheduleAvailabilityRequest.java
@@ -4,7 +4,7 @@
 
 package com.azure.resourcemanager.databox.models;
 
-import com.azure.core.annotation.Immutable;
+import com.azure.core.annotation.Fluent;
 import com.azure.core.util.logging.ClientLogger;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 /** Request body to get the availability for scheduling heavy orders. */
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "skuName")
 @JsonTypeName("DataBoxHeavy")
-@Immutable
+@Fluent
 public final class HeavyScheduleAvailabilityRequest extends ScheduleAvailabilityRequest {
     @JsonIgnore private final ClientLogger logger = new ClientLogger(HeavyScheduleAvailabilityRequest.class);
 
